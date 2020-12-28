4c77bad0d39f992fe24d28e4306b2eb83100c607
farrelpramono <76686115+farrelpramono@users.noreply.github.com>
2020-12-28 21:40:52 +0700

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,15 @@
+// function validateForm() {
+//     var x = document.forms["unit-kerja"].value;
+//     if (x == "Tata Naskah dan Kearsipan") {
+//         datalist.jabatan = ["Kepala Subbagian Tata Persuratan","Kepala Subbagian Kearsipan","PPNPN"];
+//     }
+//     if (x == "Rumah Tangga dan Perlengkapan") {
+//         datalist.jabatan = ["Kepala Bagian Rumah Tangga dan Perlengkapan","Kepala Subbagian Pengamanan dan Rumah Tangga","PPNPN"];
+//     }
+//     if (x == "Bagian Pengadaan Barang/Jasa") {
+//         datalist.jabatan = ["Kepala Bagian Pengadaan Barang/Jasa","Pengelola Pengadaan Barang/Jasa Pertama","Pengadministrasi Umum"];
+//     }
+//     else (x == "") {
+//         return false;
+//     }
+// }
